1398070f5742cb446d056812f3d3ac551c92a101
minthing <minjeongandk@gmail.com>
2022-01-30 13:19:27 +0900

    how to preload image and assets
    - preload images -> local(loadAsync) / server(prefetch) but better use loadAsync
    
    - preload fonts -> use loadsync

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
 import AppLoading from 'expo-app-loading';
 import React, {useState} from 'react';
-import { Text } from "react-native";
+import { Text, Image } from "react-native";
+// for preload fonts
+import * as Font from "expo-font";
+import {MaterialIcons} from '@expo/vector-icons';
+import { Asset } from "expo-asset";
 
 
 export default function App() {
   const [ready, setReady] = useState(false);
   const onFinish = () => setReady(true);
   const startLoading = async() => {
-    await new Promise(resolve => setTimeout(resolve, 1000))
+    await Font.loadAsync(MaterialIcons.font)
+    //local image preload
+    await Asset.loadAsync(require("./minthing.jpeg"));
+    // server image preload
+    await Image.prefetch("https://reactnative.dev/img/oss_logo.png");
+    
   };
   if(!ready){
     return <AppLoading startAsync={startLoading} onFinish={onFinish} onError={console.error}/>;
