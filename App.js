bed1aeacf3155b5f8c532c2ae63fef4d4c343cef
minthing <minjeongandk@gmail.com>
2022-01-31 11:50:42 +0900

    이미지와 폰트 불러오기 여러개 하는 법

--- App.js
+++ App.js
@@ -6,16 +6,34 @@ import * as Font from "expo-font";
 import {MaterialIcons} from '@expo/vector-icons';
 import { Asset } from "expo-asset";
 
+// hooks를 사용해서 불러올 수 있음 단, prefetch()를 사용할 수 없음.
+
+  // 폰트를 배열로 불러오는 중
+  const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font))
+  // string일 경우 prefetch() 아닐 경우 require를 사용해야 한다
+  const loadImages = (images) =>
+  images.map((image) => {
+    if (typeof image === "string") {
+      return Image.prefetch(image);
+    } else {
+      return Asset.loadAsync(image);
+    }
+  });
 
 export default function App() {
   const [ready, setReady] = useState(false);
   const onFinish = () => setReady(true);
   const startLoading = async() => {
-    await Font.loadAsync(MaterialIcons.font)
+    const fonts = loadFonts([MaterialIcons.font])
     //local image preload
-    await Asset.loadAsync(require("./minthing.jpeg"));
-    // server image preload
-    await Image.prefetch("https://reactnative.dev/img/oss_logo.png");
+    // await Asset.loadAsync(require("./minthing.jpeg"));
+    // // server image preload
+    // await Image.prefetch("https://reactnative.dev/img/oss_logo.png");
+    const images = loadImages([
+      require("./minthing.jpeg"),
+      "https://reactnative.dev/img/oss_logo.png",
+    ]);
+    await Promise.all([...fonts, ...images]);
     
   };
   if(!ready){
